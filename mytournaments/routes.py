da8ca8a4144a8ba2ae1d8e3f0fa9014495f9b83c
kappa-05 <f.cappetti.05@gmail.com>
2021-06-18 13:26:13 +0200

    Removed profile picture in registration form

--- mytournaments/routes.py
+++ mytournaments/routes.py
@@ -22,22 +22,9 @@ def register():
     
     form = RegistrationForm()
 
-    if form.validate_on_submit():
-        
-        filename = 'default.jpg'
-
-        if form.profile_picture.data:
-            token = token_hex(8)
-            _, extension = os.path.splitext(form.profile_picture.data.filename)
-            filename = token + extension
-            path = os.path.join(app.root_path, 'static/img/pfps', filename)
-
-            image = Image.open(form.profile_picture.data)
-            image.thumbnail((128, 128))
-            image.save(path)
-
+    if form.validate_on_submit():        
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password, profile_picture=filename)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
 
